1092f7d02f3fb9d1a80fc5a130822b4b275b7dd3
Elijahscriptdev <elijahobominuru@gmail.com>
2021-01-29 03:21:03 +0100

    second section setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import './App.css';
 import FirstSection from './components/sections/first-section/FirstSection';
+import SecondSection from './components/sections/second-section/SecondSection';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <FirstSection />
+      <SecondSection />
     </div>
   );
 }
---
+++ src/components/sections/second-section/SecondSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './SecondSection.css'
+
+const SecondSection = () => {
+    return (
+        <div className="second-section-container">
+            <div className="summary">
+                <div className="summary-card">jjj</div>
+                <div className="other-info">jjjj</div>
+            </div>
+            <div className="information"></div>
+        </div>
+    )
+}
+
+export default SecondSection
