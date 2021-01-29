bd4882c19be9cad27c8ff18cd0282e30e051b159
Elijahscriptdev <elijahobominuru@gmail.com>
2021-01-29 15:07:45 +0100

    styled division summary section

--- src/components/sections/second-section/SecondSection.js
+++ src/components/sections/second-section/SecondSection.js
@@ -1,16 +1,94 @@
-import React from 'react'
-import './SecondSection.css'
+import React from "react";
+import "./SecondSection.css";
 
 const SecondSection = () => {
-    return (
-        <div className="second-section-container">
-            <div className="summary">
-                <div className="summary-card">jjj</div>
-                <div className="other-info">jjjj</div>
-            </div>
-            <div className="information"></div>
+  return (
+    <div className='second-section-container'>
+      <div className='summary'>
+        <div className='summary-card'>
+          <div className='header'>
+            <img
+              src='https://res.cloudinary.com/elijjaaahhhh/image/upload/v1611919374/Icon_x0kiiw.png'
+              alt='img'
+            />
+            <h6>Division Summary</h6>
+          </div>
+          <div className='header'>
+            <img
+              src='https://res.cloudinary.com/elijjaaahhhh/image/upload/v1611924304/Vector_kidlmz.png'
+              alt='img'
+            />
+            <h6>08012345678</h6>
+          </div>
+          <div className='header'>
+            <img
+              src='https://res.cloudinary.com/elijjaaahhhh/image/upload/v1611924304/envelope_1_swiecp.png'
+              alt='img'
+            />
+            <h6>asbfefr@gmail.com</h6>
+          </div>
+          <div className='header'>
+            <img
+              src='https://res.cloudinary.com/elijjaaahhhh/image/upload/v1611924304/pin_1_wym0yt.png'
+              alt='img'
+            />
+            <h6>Mojidi, Lagos</h6>
+          </div>
+          <div className='header'>
+            <img
+              src='https://res.cloudinary.com/elijjaaahhhh/image/upload/v1611924304/notes-book_1_rr6gjx.png'
+              alt='img'
+            />
+            <h6>2 jornal Entries</h6>
+          </div>
+          <div className='header'>
+            <img
+              src='https://res.cloudinary.com/elijjaaahhhh/image/upload/v1611924304/Group_1800_c4ihp0.png'
+              alt='img'
+            />
+            <h6>24 fingerprints enrolled</h6>
+          </div>
         </div>
-    )
-}
+        <div className='other-info'>
+          <div className='header'>
+            <div className='img-container'>
+              <img
+                src='https://res.cloudinary.com/elijjaaahhhh/image/upload/v1611919379/announcement_1_ifzmxz.png'
+                alt='img'
+              />
+            </div>
 
-export default SecondSection
+            <h6>Model History</h6>
+          </div>
+
+          <div className='other-info-items'>
+            <div class='vertical_dotted_line'></div>
+            <div className='other-info-item'>
+              <span>&#8226;</span>
+              <p>Search "Journal Entries" on Division module</p>
+            </div>
+            <p className='time'>22:10 15/09/2020 . Web</p>
+          </div>
+          <div className='other-info-items'>
+            <div class='vertical_dotted_line'></div>
+            <div className='other-info-item'>
+              <span>&#8226;</span>
+              <p>Search "Journal Entries" on Division module</p>
+            </div>
+            <p className='time'>22:10 15/09/2020 . Web</p>
+          </div>
+          <div className='other-info-items'>
+            <div className='other-info-item'>
+              <span>&#8226;</span>
+              <p>Search "Journal Entries" on Division module</p>
+            </div>
+            <p className='time'>22:10 15/09/2020 . Web</p>
+          </div>
+        </div>
+      </div>
+      <div className='information'>jjj</div>
+    </div>
+  );
+};
+
+export default SecondSection;
